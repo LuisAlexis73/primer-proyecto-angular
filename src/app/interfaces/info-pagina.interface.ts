0316acf498819cbf8d6669840d945b41fc99a8a0
Alexis Galarza <colo1990.ag@gmail.com>
2022-12-11 23:34:56 -0300

    agregado de interfaz para comenzar a consumir los datos desde el json que tenemos credo en la carpeta data

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+
+export interface InfoPagina {
+	titulo?: string;
+	email?: string;
+	nombre?: string;
+	github?: string;
+	faceboock?: string;
+	instagram?: string;
+	'equipo de trabajo'?: any[];
+}
